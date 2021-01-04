44e82563aa9122150867344995eb7b4e36e3adc6
Nikki <piknik@me.com>
2021-01-04 11:29:53 -0700

    working on favorites

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ getAnswerButton.addEventListener('click', function(e) {
   document.querySelector('.give-answer').innerHTML = `
     <div class="question"><em>${userInput.value}?</em></div>
     <div class="answer" style="font-size:1.2rem;font-weight:bold;"><strong>${response}</strong></div>
+    <button class="fave-btn">Favorite</button>
   `
 
   userInput.value = "";
@@ -25,6 +26,15 @@ clearButton.addEventListener('click', function(e) {
   document.querySelector('.ball').classList.toggle('hidden');
   document.querySelector('.give-answer').classList.toggle('hidden');
   document.querySelector('.clear').disabled = true;
-})
+});
 
+var faveButton = document.querySelector('.fave-btn');
+faveButton.addEventListener('click', function(e) {
+  e.preventDefault();
+
+  document.querySelector('.fave-list').classList.toggle('.hidden');
+  document.querySelector('.faves-list').innerHTML = `
+      <li><em>${userInput.value}?</em> -- <strong>${response}</strong></li>
+    `
+});
 
