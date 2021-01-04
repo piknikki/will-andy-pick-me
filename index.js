11ea8dddeed172eb9489773162b668df7113301a
Nikki <piknik@me.com>
2021-01-04 10:00:29 -0700

    comp of basic layouts is right move onto js functionality

---
+++ index.js
@@ -0,0 +1,10 @@
+// get input
+
+// get random response
+
+// inject that into response and hide the ball
+// should have question in italics and answer in slightly larger font (maybe bold?)
+// should also remove property of disabled from button
+
+
+
